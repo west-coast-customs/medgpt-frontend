bdfe77b18def94e2dc07ab3151c786d6805d8a3e
Ilia Mishchenko <ilia.mishchenko@eltex.loc>
2024-10-15 02:49:16 +0700

    Implemented landing page

---
+++ src/app/components/card/card.component.ts
@@ -0,0 +1,14 @@
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class CardComponent {
+  @Input() title!: string;
+  @Input() text!: string;
+}
