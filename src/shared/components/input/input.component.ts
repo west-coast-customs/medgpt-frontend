6fd4b81e30432fdab0397e5d10d51dd97b73ac47
Ilia Mishchenko <ilia.mishchenko@eltex.loc>
2024-11-07 18:10:45 +0700

    feat * Auth Добавлен кастомный форм контрол для инпута

--- src/shared/components/input/input.component.ts
+++ src/shared/components/input/input.component.ts
@@ -1,14 +1,17 @@
 import {
   ChangeDetectionStrategy,
   Component,
+  effect,
   ElementRef,
   input,
   InputSignal,
-  output,
-  OutputEmitterRef,
-  viewChild
+  model,
+  ModelSignal,
+  signal,
+  viewChild,
+  WritableSignal
 } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { ControlValueAccessor, FormsModule, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { NgClass } from '@angular/common';
 
 @Component({
@@ -17,19 +20,52 @@ import { NgClass } from '@angular/common';
   imports: [FormsModule, NgClass],
   templateUrl: './input.component.html',
   styleUrl: './input.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [{ provide: NG_VALUE_ACCESSOR, useExisting: InputComponent, multi: true }]
 })
-export class InputComponent {
-  disabled: InputSignal<boolean> = input<boolean>(false)
+export class InputComponent implements ControlValueAccessor {
   placeholder: InputSignal<string> = input<string>('')
   customClass: InputSignal<string> = input<string>('')
+  type = input<'text' | 'email' | 'password'>('text')
 
-  inputText: InputSignal<string> = input<string>('')
-  inputTextChange: OutputEmitterRef<string> = output<string>()
+  disabled: InputSignal<boolean> = input<boolean>(false)
+  disabledForm: WritableSignal<boolean> = signal(false)
+
+  value: ModelSignal<string> = model('')
+  onChange: ((value: string) => void) | undefined
+  onTouched: (() => void) | undefined
+
+  constructor() {
+    effect(() => {
+      if (!this.disabled() && !this.disabledForm()) {
+        this.onChange?.(this.value())
+      }
+    });
+  }
+
+  writeValue(newValue: string): void {
+    this.value.set(newValue)
+  }
+
+  registerOnChange(fn: (value: string) => void): void {
+    this.onChange = fn
+  }
+
+  registerOnTouched(fn: () => void): void {
+    this.onTouched = fn
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+    this.disabledForm.set(isDisabled)
+  }
 
   inputElement = viewChild<ElementRef<HTMLInputElement>>('inputElement')
 
   focus(): void {
     setTimeout(() => this.inputElement()?.nativeElement.focus())
+  }
+
+  onInputFocus(): void {
+    this.onTouched?.()
   }
 }
