834e18c8260cef562db782993839f6eeb6be0952
Ilia Mishchenko <ilia.mishchenko@eltex.loc>
2024-11-24 15:09:14 +0700

    bug * Logout
    Добавлена отложенная загрузка нотификаций

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ToastsComponent } from '../shared/components/toasts/toasts.component';
+import { ToastsService } from '../shared/services/toasts.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   template: `
     <div class="w-full h-full">
-      <app-toasts/>
+      @defer (when toastsService.toasts().length) {
+        <app-toasts/>
+      }
       <router-outlet/>
     </div>`,
   imports: [RouterOutlet, ToastsComponent]
 })
-export class AppComponent {}
+export class AppComponent {
+  toastsService: ToastsService = inject(ToastsService);
+}
