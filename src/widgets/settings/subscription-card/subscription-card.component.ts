a6ea776de020a4885d0d177d8c2079f6aac70338
Ilia Mishchenko <ilia.mishchenko@eltex.loc>
2025-01-04 16:54:02 +0700

    feat * Settings Добавлен лоадер для запроса на подписку

--- src/widgets/settings/subscription-card/subscription-card.component.ts
+++ src/widgets/settings/subscription-card/subscription-card.component.ts
@@ -18,14 +18,19 @@ import { ActivatedRoute } from '@angular/router';
 import { DatePipe } from '@angular/common';
 import { ButtonComponent } from '../../../shared/components/button/button.component';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { LoaderComponent } from '../../../shared/components/loader/loader.component';
+import { of, switchMap, tap } from 'rxjs';
+import { fadeOnEnter } from '../../../shared/utils/animations';
 
 @Component({
   selector: 'app-subscription-card',
   standalone: true,
   imports: [
     DatePipe,
-    ButtonComponent
+    ButtonComponent,
+    LoaderComponent
   ],
+  animations: [fadeOnEnter(300)],
   templateUrl: './subscription-card.component.html',
   styleUrl: './subscription-card.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
@@ -35,12 +40,20 @@ export class SubscriptionCardComponent {
 
   settings: WritableSignal<Profile> = signal(inject(ActivatedRoute).snapshot.data?.['settings'])
   subscriptionStatus: Signal<string | undefined> = computed(() => SUBSCRIPTION_STATUSES_MAP.get(this.settings().subscription.status))
+  subscriptionPlanLoading: WritableSignal<boolean> = signal(false)
 
   constructor(private profileService: ProfileService, private destroyRef: DestroyRef) {}
 
-  onSubscriptionPlanClick(period: SubscriptionPeriods) {
-    this.profileService.activateSubscription(period)
-      .pipe(takeUntilDestroyed(this.destroyRef))
+  onSubscriptionPlanClick(period: SubscriptionPeriods): void {
+    of(this.subscriptionPlanLoading.set(true))
+      .pipe(
+        switchMap(() => this.profileService.activateSubscription(period)),
+        tap({
+          next: () => this.subscriptionPlanLoading.set(false),
+          error: () => this.subscriptionPlanLoading.set(false)
+        }),
+        takeUntilDestroyed(this.destroyRef)
+      )
       .subscribe()
   }
 }
